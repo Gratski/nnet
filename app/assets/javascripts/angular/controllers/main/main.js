936d3bb2876056c370eff8b1a026abcba6a7efee
Joao Rodrigues <JoaoRodrigues@Joaos-Air-2.lan>
2016-02-08 15:21:52 +0000

    fix angular min issue

--- app/assets/javascripts/angular/controllers/main/main.js
+++ app/assets/javascripts/angular/controllers/main/main.js
@@ -1,9 +1,9 @@
 (function(){
 
-  var controller = function($scope){
-    $scope.username = 'Nome de user'
-  }
-
-  angular.module('social').controller('main', controller);
+  angular
+    .module('social')
+    .controller('main', ['$scope', function($scope) { 
+        //your minsafe controller 
+    }]);
 
 }())
